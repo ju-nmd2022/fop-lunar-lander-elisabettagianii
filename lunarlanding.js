b01c3f25bd8b8ed6f4be317d813baa8a093e24eb
elisabettagianii <elisabettagiani@yahoo.com>
2023-03-03 12:23:26 +0100

    i did all the 3 screens
    i might move the landing place and make the screen work one after another

--- lunarlanding.js
+++ lunarlanding.js
@@ -1,10 +1,101 @@
+let rocketY = 100;
+let rocketX = 400;
+let velocity = 0.1;
+let acceleration = 0.1;
+let isGameActive = true;
+
 // creating the canva
 function setup() {
   createCanvas(800, 800);
+  // creating the button
+  button = createButton("START");
+  button.position(400, 400);
+  button.style("color:white");
+  button.style("background-color:black");
+  button.style("border", "none");
+  button.size(100, 50);
+  button.hide();
 }
 
 background(0, 0, 0);
+// stars
+let starX = [];
+let starY = [];
+let starAlpha = [];
 
+for (let i = 0; i < 152; i++) {
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  const alpha = Math.random();
+
+  starX.push(x);
+  starY.push(y);
+  starAlpha.push(alpha);
+}
+
+// screen 1
+function screen1() {
+  // sky with stars. source:Garrit's videolecture
+  noStroke();
+  background(0, 0, 0);
+
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 4);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+  fill(255, 255, 255);
+  textSize(70);
+  text("ASTRO GAME", 100, 300, 900, 500);
+  button.show();
+}
+// sky with stars. source:Garrit's videolecture
+function screen2() {
+  noStroke();
+  background(0, 0, 0);
+
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 4);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+}
+// screen for when you win the game
+function screenWin() {
+  noStroke();
+  background(0, 0, 0);
+
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 4);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+  fill(255, 255, 255);
+  textSize(70);
+  text("YOU WON!", 200, 300, 900, 500);
+}
+// screen for when you loose the game
+function screenLost() {
+  noStroke();
+  background(0, 0, 0);
+
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 4);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+  fill(255, 255, 255);
+  textSize(70);
+  text("YOU LOST!", 200, 300, 900, 500);
+}
+// ground
+function ground(x, y) {
+  noStroke();
+  fill(211, 211, 211);
+  rect(x, y, 800, 500);
+  fill(0, 0, 0);
+  ellipse(400, 700, 200, 40);
+}
 // making the rocket using part of the code from the ufo of the Garrit's exemple
 function rocket(x, y) {
   push();
@@ -58,39 +149,57 @@ function rocket(x, y) {
 
   pop();
 }
+// source: modified from Garrit's videolecture
+state = "start";
 
 function draw() {
-  background(255, 255, 255);
-  rocket(400, 600);
+  // screen 1
+  if (state === "start") {
+    screen1();
+    ground(0, 630);
+  }
+  if (state === "play") {
+    // screen 2
+    background(255, 255, 255);
+    screen2();
+    ground(0, 630);
+    rocket(rocketX, rocketY);
+
+    if (isGameActive) {
+      rocketY = rocketY + velocity;
+      velocity = velocity + acceleration;
+    }
+
+    if (rocketY > 610) {
+      isGameActive = false;
+    }
+    // make the velocity change if you click the up arrow
+    if (keyIsDown(38)) {
+      velocity = velocity - 0.3;
+    }
+    if (keyIsDown(37)) {
+      rocketX = rocketX - 4;
+    }
+    if (keyIsDown(39)) {
+      rocketX = rocketX + 4;
+    }
+  }
+  if (rocketX > 350 && rocketX < 450 && rocketY > 604) {
+    state = "win";
+  }
+  if (state === "win") {
+    screenWin();
+    ground(0, 630);
+    rocket(400, 610);
+  }
+  if (rocketX > 0 && rocketX < 300 && rocketY > 604) {
+    state = "lost";
+  }
+  if (rocketX > 500 && rocketX < 800 && rocketY > 604) {
+    state = "lost";
+  }
+  if (state === "lost") {
+    screenLost();
+    ground(0, 630);
+  }
 }
-
-function ground (x, y) {
-  fill(211, 211, 211);
-  rect(x, y, 800, 500);
-}
-
-// // sky with stars. source:Garrit's videolecture
-// let starX = [];
-// let starY = [];
-// let starAlpha = [];
-
-// for (let i = 0; i < 152; i++) {
-//   const x = Math.floor(Math.random() * width);
-//   const y = Math.floor(Math.random() * height);
-//   const alpha = Math.random();
-
-//   starX.push(x);
-//   starY.push(y);
-//   starAlpha.push(alpha);
-// }
-
-// function draw() {
-//   noStroke();
-//   background(0, 0, 0);
-
-//   for (let index in starX) {
-//     fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
-//     ellipse(starX[index], starY[index], 4);
-//     starAlpha[index] = starAlpha[index] + 0.02;
-//   }
-// }
